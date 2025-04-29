0a68175ff057902c294221c113baaf8a67ed7bf0
Glauco Vitti Bonturi <gvittibonturi@gmail.com>
2025-04-29 08:36:23 -0300

    Classe construida

--- cadastro.py
+++ cadastro.py
@@ -8,40 +8,45 @@ from datetime import datetime
 cartoes = ['R$50,00', 'R$250,00', 'R$120,00']
 
 usuarios = []
-#info_usuario = {}
 
-while True:
-    valor_nome = input('Digite o nome do usuário: ')    
-    if valor_nome == 'q':
-        print('Programa finalizado')
-        print(usuarios)     
-        break # Interrompe o laço se o usuário digitar 'q'
+class User():
+    """Classe que cria e define o usuário de um sistema"""
 
-    try:
-        valor_idade = (input('Digite a idade do usuário: '))
-        str_aniversario = input('Digite a data de aniversário: ')
-        data_niver_convert = datetime.strptime(str_aniversario, '%d/%m/%Y') # registra a data digitada do aniversario do usuario e converte para TIME
-        data_niver_br = data_niver_convert.strftime('%d/%m;%Y') # converte a variável 'data_niver_convert' para padrão brasileiro
-        data_cad_convert = datetime.now().strftime('%d/%m/%Y') # registra a data do cadastro do usuário
+    def __init__(self, nome, idade, data_aniversario, data_cadastro):
+        """Inicializa os atributos nome, idade, data_aniversario e data_cadastro"""
+        self.nome = nome
+        self.idade = idade
+        self.data_aniversario = data_aniversario
+        self.data_cadastro = data_cadastro
 
-        info_usuario = {'Nome': valor_nome, 'idade': valor_idade, 'data_aniversario': data_niver_br,
-                         'data_cadastro': data_cad_convert}
+    def cadastrar_usuario(self):
+        """Cadastra novos usuários"""   
+        while True:
+            valor_nome = input('Digite o nome do usuário: ')    
+            if valor_nome == 'q':
+                print('Programa finalizado')
+                print(usuarios)     
+                break # Interrompe o laço se o usuário digitar 'q'
+
+
+
+#     try:
+#         valor_idade = (input('Digite a idade do usuário: '))
+#         str_aniversario = input('Digite a data de aniversário: ')
+#         data_niver_convert = datetime.strptime(str_aniversario, '%d/%m/%Y') # registra a data digitada do aniversario do usuario e converte para TIME
+#         data_niver_br = data_niver_convert.strftime('%d/%m;%Y') # converte a variável 'data_niver_convert' para padrão brasilei
+#         data_cad_convert = datetime.now().strftime('%d/%m/%Y') # registra a data do cadastro do usuário
+
+#         info_usuario = {'Nome': valor_nome, 'idade': valor_idade, 'data_aniversario': data_niver_br,
+#                          'data_cadastro': data_cad_convert}
         
-        usuarios.append(info_usuario)
+#         usuarios.append(info_usuario)
 
-    except ValueError:
-        print('Entrada inválida. Digite um nome válido.')
+#     except ValueError:
+#         print('Entrada inválida. Digite um nome válido.')
 
-    #print(f'Data do cadastro: {data_cadastro}')
-    #cartao_usuario = random.choice(cartoes)
-else:
-    print('Programa finalilzado')
+#     #print(f'Data do cadastro: {data_cadastro}')
+#     #cartao_usuario = random.choice(cartoes)
+# else:
+#     print('Programa finalilzado')
 
-
-
-'''
-nome_busca = input('Digite o nome que deseja buscar: ')
-for i in range(0, len(usuarios)):
-    user = usuarios[i]
-    print(user)
-'''
